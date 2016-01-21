50b6d57ed4788f9e9d4a83ce26d9ae0447cf5489
Taylor Pokoj and Will Johnson <trashmoldwilliams@gmail.com>
2016-01-21 10:32:04 -0800

    Adds functionality to all tests, returning false if words, blanks, or negative numbers are entered.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,15 @@
 var countUpBy = function (total, multiple) {
 
+  if ((isNaN(total) || isNaN(multiple)) || (total === "" || multiple === "") || (total <= 0 || multiple <= 0)) {
+   alert("Please enter numbers in both fields!");
+   return false;
+ } else {
   var arrayOutput = [];
   for (var i = multiple; i <= total; i += multiple) {
     arrayOutput.push(i);
   };
   return arrayOutput;
+ }
 };
 
 
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,18 @@
 describe("countUpBy", function() {
-  it("will count to total by multiple input", function() {
+
+  it("will count to total by multiple input, and if the remainder is not zero, it stops at the closest multiple under the total", function() {
     expect(countUpBy(30, 5)).to.eql([5, 10, 15, 20, 25, 30]);
+  });
+
+  it("will return false if either input is not a number", function() {
+    expect(countUpBy("apple", 8)).to.equal(false);
+  });
+
+  it("will return false if either input is left blank", function() {
+    expect(countUpBy("", 6)).to.equal(false);
+  });
+
+  it("will return false if either input is 0 or negative", function() {
+    expect(countUpBy(-1, 6)).to.equal(false);
   });
 });
