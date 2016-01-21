b0d59711fed64dfe2e0a328dfcb53be7824fd772
Taylor Pokoj and Will Johnson <trashmoldwilliams@gmail.com>
2016-01-21 11:31:26 -0800

    Add functionality to all tests and user interface result output

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-var countUpBy = function (total, multiple) {
+
+var countUpBy = function(total, multiple) {
 
   if ((isNaN(total) || isNaN(multiple)) || (total === "" || multiple === "") || (total <= 0 || multiple <= 0)) {
    alert("Please enter numbers in both fields!");
@@ -13,9 +14,16 @@ var countUpBy = function (total, multiple) {
 };
 
 
-// $(document).ready(function() {
-//   $("form#countUpBy").submit(function(event) {
-//
-//     var total = $("input#total").val();
-//     var multiple = $("input#multiple").val();
-//
+$(document).ready(function() {
+  $("form#countUpBy").submit(function(event) {
+
+    var total = parseInt($("input#total").val());
+    var multiple = parseInt($("input#multiple").val());
+    var result = countUpBy(total, multiple);
+    for (i = 0; i < result.length; i++) {
+      $("#result").append("<li>" + result[i] + "</li>");
+    };
+
+    event.preventDefault();
+  });
+});
